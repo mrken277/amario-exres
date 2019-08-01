3bb6db401bfb91daacab76a7dd3032a9415b8f76
Ujin's iMac <anu.b@nmma.co>
2019-08-01 19:04:28 +0800

    Remove some duplicates

--- src/modules/tasks/components/PortableTasks.tsx
+++ src/modules/tasks/components/PortableTasks.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import options from '../options';
 
 type IProps = {
+  customerIds?: string[];
+  companyIds?: string[];
   contentType?: string;
   contentId?: string;
   isOpen?: boolean;
