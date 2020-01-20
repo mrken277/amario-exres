c44428394c631134a503e202f7c89b200ac7ffe9
anu-ujin <anu.b@nmma.co>
2020-01-20 16:41:03 +0800

    fix some declaring

--- ui/src/modules/settings/general/containers/List.tsx
+++ ui/src/modules/settings/general/containers/List.tsx
@@ -11,74 +11,70 @@ import {
   ConfigsInsertMutationVariables
 } from '../types';
 
-class ListContainer extends React.PureComponent<{}> {
-  render() {
-    const {
-      loading: currencyConfigQueryLoading,
-      error: currencyConfigQueryError,
-      data: currencyConfigQueryData
-    } = useQuery<ConfigDetailQueryResponse, { code: string }>(
-      gql(queries.configsDetail),
-      {
-        fetchPolicy: 'network-only',
-        variables: {
-          code: 'dealCurrency'
-        }
+export default () => {
+  const {
+    loading: currencyConfigQueryLoading,
+    error: currencyConfigQueryError,
+    data: currencyConfigQueryData
+  } = useQuery<ConfigDetailQueryResponse, { code: string }>(
+    gql(queries.configsDetail),
+    {
+      fetchPolicy: 'network-only',
+      variables: {
+        code: 'dealCurrency'
       }
-    );
+    }
+  );
 
-    const {
-      loading: uomConfigQueryLoading,
-      error: uomConfigQueryError,
-      data: uomConfigQueryData
-    } = useQuery<ConfigDetailQueryResponse, { code: string }>(
-      gql(queries.configsDetail),
-      {
-        fetchPolicy: 'network-only',
-        variables: {
-          code: 'dealUOM'
-        }
+  const {
+    loading: uomConfigQueryLoading,
+    error: uomConfigQueryError,
+    data: uomConfigQueryData
+  } = useQuery<ConfigDetailQueryResponse, { code: string }>(
+    gql(queries.configsDetail),
+    {
+      fetchPolicy: 'network-only',
+      variables: {
+        code: 'dealUOM'
       }
+    }
+  );
+
+  const [mutate, { error: insertConfigError }] =
+    useMutation<ConfigsInsertMutationResponse, ConfigsInsertMutationVariables>(
+      gql(mutations.insertConfig), { refetchQueries: ['configsDetail'] }
     );
 
-    const [mutate, { error: insertConfigError }] =
-      useMutation<ConfigsInsertMutationResponse, ConfigsInsertMutationVariables>(
-        gql(mutations.insertConfig), { refetchQueries: ['configsDetail'] }
-      );
+  const save = (code, value) => {
+    mutate({ variables: { code, value } })
+  };
 
-    const save = (code, value) => {
-      mutate({ variables: { code, value } })
-    };
-
-    if (currencyConfigQueryLoading || uomConfigQueryLoading) {
-      return <Spinner objective={true} />;
-    }
-
-    if (currencyConfigQueryError || uomConfigQueryError || insertConfigError) {
-      return <p>Loading...</p>;
-    }
-
-    const currencies = currencyConfigQueryData && currencyConfigQueryData.configsDetail;
-    const uom = uomConfigQueryData && uomConfigQueryData.configsDetail;
-
-    const updatedProps = {
-      currencies: currencies ? currencies.value : [],
-      uom: uom ? uom.value : [],
-      save
-    };
-
-    return (
-      <AppConsumer>
-        {({ currentLanguage, changeLanguage }) => (
-          <List
-            {...updatedProps}
-            currentLanguage={currentLanguage}
-            changeLanguage={changeLanguage}
-          />
-        )}
-      </AppConsumer>
-    );
+  if (currencyConfigQueryLoading || uomConfigQueryLoading) {
+    return <Spinner objective={true} />;
   }
-}
 
-export default ListContainer;
+  if (currencyConfigQueryError || uomConfigQueryError || insertConfigError) {
+    return <p>Loading...</p>;
+  }
+
+  const currencies = currencyConfigQueryData && currencyConfigQueryData.configsDetail;
+  const uom = uomConfigQueryData && uomConfigQueryData.configsDetail;
+
+  const updatedProps = {
+    currencies: currencies ? currencies.value : [],
+    uom: uom ? uom.value : [],
+    save
+  };
+
+  return (
+    <AppConsumer>
+      {({ currentLanguage, changeLanguage }) => (
+        <List
+          {...updatedProps}
+          currentLanguage={currentLanguage}
+          changeLanguage={changeLanguage}
+        />
+      )}
+    </AppConsumer>
+  );
+}
