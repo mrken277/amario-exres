6b05cac5dbca69541b2abbc2c85f05d0aa4f9e62
anu-ujin <anu.b@nmma.co>
2020-02-05 16:58:50 +0800

    add properties

--- ui/src/modules/testing-utils/factories/settings/permissions.ts
+++ ui/src/modules/testing-utils/factories/settings/permissions.ts
@@ -1,6 +1,19 @@
 import * as Factory from 'factory.ts';
-import { IPermission, IPermissionDocument, IUserGroup } from 'modules/settings/permissions/types';
+import { IActions, IModule, IPermission, IPermissionDocument, IUserGroup } from 'modules/settings/permissions/types';
 import { userFactory } from '../auth';
+
+export const actionsFactory = Factory.Sync.makeFactory<IActions>({
+  name: 'nmma',
+  module: 'module',
+  description: 'desc',
+  use: ['']
+});
+
+export const moduleFactory = Factory.Sync.makeFactory<IModule>({
+  name: 'nmma',
+  description: 'desc',
+  actions: [actionsFactory.build()]
+});
 
 export const userGroupFactory = Factory.Sync.makeFactory<IUserGroup>({
   _id: '1',
