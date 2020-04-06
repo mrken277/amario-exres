ec01777e9702270bb47a4467d3ed72da7beee0c4
BatAmar Battulga <batamar.b@nmtec.co>
2020-04-06 23:27:47 +0800

    test fix

--- ui/src/modules/segments/containers/__tests__/SegmentsList.test.tsx
+++ ui/src/modules/segments/containers/__tests__/SegmentsList.test.tsx
@@ -1,35 +1,45 @@
 import { MockedProvider } from '@apollo/react-testing';
-import { GraphQLError } from 'graphql';
 import gql from 'graphql-tag';
 import { segmentFactory } from 'modules/testing-utils/factories/segments';
 import { withRouter } from 'modules/testing-utils/withRouter';
 import * as React from 'react';
-import { create } from 'react-test-renderer';
+import * as renderer from 'react-test-renderer';
 import wait from 'waait';
-import { mutations, queries } from '../../graphql';
 import SegmentListContainer from '../SegmentsList';
 
 const contentType = 'customer';
 const segmentVariables = { contentType };
 
-const segmentsErrorMock = {
-  request: {
-    query: gql(queries.segments),
-    variables: segmentVariables
-  },
-  result: {
-    errors: [new GraphQLError('forced error')]
-  }
-};
+const segmentFields = `
+  _id
+  contentType
+  name
+  description
+  subOf
+  color
+  conditions
+`;
 
-const configQueryMock = {
+const segments1 = `
+  query segments($contentType: String!) {
+    segments(contentType: $contentType) {
+      ${segmentFields}
+
+      getSubSegments {
+        ${segmentFields}
+      }
+    }
+  }
+`;
+
+const segmentsQueryMock = {
   request: {
-    query: gql(queries.segments),
+    query: gql(segments1),
     variables: segmentVariables
   },
   result: {
     data: {
-      configsDetail: [
+      segments: [
         segmentFactory.build(),
         segmentFactory.build({
           _id: 'id'
@@ -39,65 +49,16 @@ const configQueryMock = {
   }
 };
 
-const removeSegmentMutationMocks = {
-  request: {
-    query: gql(mutations.segmentsRemove),
-    variables: { _id: '' }
-  },
-  result: {
-    data: {
-      _id: ''
-    }
-  }
-};
+it('SegmentsList', async () => {
+  const component = renderer.create(
+    <MockedProvider mocks={[segmentsQueryMock]}>
+      {withRouter(<SegmentListContainer contentType={contentType} />)}
+    </MockedProvider>
+  );
 
-describe('SegmentsList', () => {
-  it('should render loading state initially', () => {
-    const testRenderer = create(
-      <MockedProvider mocks={[]}>
-        <SegmentListContainer contentType="" />
-      </MockedProvider>
-    );
+  await renderer.act(wait);
 
-    const testInstance = testRenderer.root;
-    const loader = testInstance.findByProps({ objective: true }).type;
+  const testInstance = component.root;
 
-    const spinner = loader({});
-
-    expect(spinner.props.objective).toEqual(false);
-  });
-
-  it('error', async () => {
-    const testRenderer = create(
-      <MockedProvider mocks={[segmentsErrorMock]} addTypename={false}>
-        <SegmentListContainer contentType={contentType} />
-      </MockedProvider>
-    );
-
-    await wait(0);
-
-    const testInstance = testRenderer.root;
-    const span = testInstance.findByType('span');
-    expect(span.children).toContain('forced error');
-  });
-
-  it('should render content', async () => {
-    const testRenderer = create(
-      <MockedProvider
-        mocks={[configQueryMock, removeSegmentMutationMocks]}
-        addTypename={false}
-      >
-        {withRouter(<SegmentListContainer contentType={contentType} />)}
-      </MockedProvider>
-    );
-
-    await wait(0); // wait for response
-
-    const testInstance = testRenderer.root;
-    expect(testRenderer.toJSON()).toMatchSnapshot();
-
-    expect(
-      testInstance.findByType(SegmentListContainer).props.contentType
-    ).toBe(contentType);
-  });
+  expect(testInstance.findByType('button').props.icon).toBe('plus-circle');
 });
