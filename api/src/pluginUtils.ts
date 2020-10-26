864d473fce5a17ffb7b54a747b70c5eb9714c431
BatAmar Battulga <batamar.b@nmtec.co>
2020-10-26 14:52:30 +0800

    remove space

--- api/src/pluginUtils.ts
+++ api/src/pluginUtils.ts
@@ -64,7 +64,7 @@ export const execInEveryPlugin = (callback) => {
                     graphqlSchema,
                     graphqlQueries,
                     graphqlMutations,
-                    models 
+                    models
                 })
             })
         })
