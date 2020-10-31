0e4c4cae71ffd91e2c629fe7a03ccc4b672b2004
BatAmar Battulga <batamar.b@nmtec.co>
2020-10-31 20:35:47 +0800

    bin

--- bin/erxes.js
+++ bin/erxes.js
@@ -1,43 +1,4 @@
 #!/usr/bin/env node
 'use strict';
 
-const _ = require('lodash');
-
-const program = require('commander');
-const packageJSON = require('../package.json');
-const buildCmd = require('../ commands/build');
-
-/**
- * Normalize version argument
- *
- * `$ erxes -v`
- * `$ erxes -V`
- * `$ erxes --version`
- * `$ erxes version`
- */
-
-program.allowUnknownOption(true);
-
-// Expose version.
-program.version(packageJSON.version, '-v, --version');
-
-// Make `-v` option case-insensitive.
-process.argv = _.map(process.argv, arg => {
-  return arg === '-V' ? '-v' : arg;
-});
-
-// `$ erxes version` (--version synonym)
-program
-  .command('version')
-  .description('output your version of Erxes')
-  .action(() => {
-    console.log(packageJSON.version);
-  });
-
-// `$ erxes build`
-program
-  .command('build')
-  .description('Build your Erxes application in production mode')
-  .action(buildCmd);
-
-program.parse(process.argv);
+console.log('mmmmmmmmmmmm');
