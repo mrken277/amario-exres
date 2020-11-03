3bee1f8d0f6fdca961c40818efca711dfb7516fe
BatAmar Battulga <batamar.b@nmtec.co>
2020-11-03 19:49:39 +0800

    reduce build size

--- commands/build-repos.js
+++ commands/build-repos.js
@@ -5,7 +5,7 @@ const fs = require('fs-extra');
 const build = async function() {
   // build api
   process.chdir(path.resolve(__dirname, '..', 'api'));
-  await shell.exec('yarn install');
+  await shell.exec('yarn install --production');
   await shell.exec('yarn build');
   await shell.exec('cp -rf src/private ../build/api');
   await shell.exec('cp -rf node_modules ../build/api');
@@ -13,38 +13,38 @@ const build = async function() {
 
   // build integrations
   process.chdir(path.resolve(__dirname, '..', 'integrations'));
-  await shell.exec('yarn install');
+  await shell.exec('yarn install --production');
   await shell.exec('yarn build');
   await shell.exec('cp -rf node_modules ../build/integrations');
 
   // build engages
   process.chdir(path.resolve(__dirname, '..', 'engages-email-sender'));
-  await shell.exec('yarn install');
+  await shell.exec('yarn install --production');
   await shell.exec('yarn build');
   await shell.exec('cp -rf node_modules ../build/engages');
 
   // build email verifier
   process.chdir(path.resolve(__dirname, '..', 'email-verifier'));
-  await shell.exec('yarn install');
+  await shell.exec('yarn install --production');
   await shell.exec('yarn build');
   await shell.exec('cp -rf node_modules ../build/email-verifier');
 
   // build logger
   process.chdir(path.resolve(__dirname, '..', 'logger'));
-  await shell.exec('yarn install');
+  await shell.exec('yarn install --production');
   await shell.exec('yarn build');
   await shell.exec('cp -rf node_modules ../build/logger');
 
   // build ui
   process.chdir(path.resolve(__dirname, '..', 'ui'));
-  await shell.exec('yarn install');
+  await shell.exec('yarn install --production');
   await shell.exec('yarn build');
 
   fs.move(path.resolve(__dirname, '..', 'ui/build'), path.resolve(__dirname, '..', 'build/ui'));
 
   // build widgets
   process.chdir(path.resolve(__dirname, '..', 'widgets'));
-  await shell.exec('yarn install');
+  await shell.exec('yarn install --production');
   await shell.exec('yarn build');
 
   fs.copy(path.resolve(__dirname, '..', 'widgets'), path.resolve(__dirname, '..', 'build/widgets'));
